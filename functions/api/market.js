f76d4377f97326aef47ce56252d6d6fd8ef8d0de
seksanhompes <38927849+seksanhompes@users.noreply.github.com>
2025-08-26 06:06:37 +0700

    Delete functions/api/market.js

--- functions/api/market.js
+++
@@ -1,6 +0,0 @@
-// functions/api/market.js
-export async function onRequest() {
-  return new Response(JSON.stringify({ ok: true, ping: 'market' }), {
-    headers: { 'Content-Type': 'application/json' }
-  });
-}
